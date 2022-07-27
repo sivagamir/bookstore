a7a99d47844811de21d5a42d0a4dc903d40addda
Sivagami R <105543275+sivagamir@users.noreply.github.com>
2022-07-27 23:42:29 +0530

    Add files via upload

---
+++ bookstoreproject/bookstoreproject/urls.py
@@ -0,0 +1,39 @@
+"""bookstoreproject URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+]
+
+
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from bookApp.views import *
+
+urlpatterns =[
+    path('admin/', admin.site.urls),
+    
+    path('', home,name='home'),
+    path('login/', loginPage,name='login'),
+    path('viewcart/', viewcart,name='viewcart'),
+    path('addbook/', addbook,name='addbook'),
+    path('register/', registerPage,name='register'),
+    path('logout/', logoutPage,name='logout'),
+    path('addtocart/<str:pk>', addtocart,name='addtocart'),
+]
